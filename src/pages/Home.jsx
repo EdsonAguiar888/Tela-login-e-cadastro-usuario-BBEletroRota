778daf857176d9513065bae200c441614b6f9ed4
EdsonAguiar888 <edsonaguiar888@gmail.com>
2026-05-08 01:14:07 -0300

    teste

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 
-import './home.css';
+import './Home.css';
 import imagemCarro from '../assets/imagemCarro.png';
 
 export default function Home({ usuario, setUsuario }) {
